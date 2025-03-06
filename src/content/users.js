c28f486e742d4053dd9fb8a3b2cec1f411fa6c97
Stetsiuk Oleksandr <StetONN155@gmail.com>
2025-03-06 21:56:19 +0200

    update search cards

--- src/content/users.js
+++ src/content/users.js
@@ -11,49 +11,9 @@ async function showCards({ input }) {
         return;
     }
 
-    changeCards(cards, cards.userCards, cardsFinder.rank, cardsFinder.src);
+    changeCards(cards);
 
     ShowBar.addElementsToBar(cards.getCardsArray());
-}
-
-function changeCards(cards, myCards, rank, src) {
-    cards.forEach(tradeCard => {
-        tradeCard.addEventListener('click', async () => {
-            const getCard = new GetCards({ userUrl: tradeCard.url, userName: tradeCard.userName, rank });
-            const userInventory = await getCard.getInventory();
-            const card = userInventory.find(card => card.src === src);
-            const myCard = myCards.find(card => card.src === tradeCard.src);
-            
-            let text;
-            let disabled = false;
-
-            if (!card) {
-                text = "In trade or not found";
-                disabled = true;
-            }
-            else if (tradeCard.rate < 0) {
-                text = "This card is locked";
-                disabled = true;
-            }
-            else if (myCard.lock === "trade") {
-                text = "This card is in trade";
-                disabled = true;
-            }
-            else {
-                text = `${myCard.lock === "lock" ? "Unlock and ": ""}Trade`
-            }
-
-            const button = new Button({
-                disabled,
-                text,
-                onClick: async () => {
-                    await trade(card, myCard);
-                }
-            });
-            
-            await button.asyncPlace(".anime-cards__controls")
-        })
-    })
 }
 
 async function checkUserCards(user, rank = "s") {
