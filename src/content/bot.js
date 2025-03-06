61ba7c9e375b60271929f78ca9d310ee74e1c05d
Stetsiuk Oleksandr <StetONN155@gmail.com>
2025-03-06 16:51:18 +0200

    bot changes

--- src/content/bot.js
+++ src/content/bot.js
@@ -12,30 +12,39 @@ socket.on("searchCards", async (data) => {
 
     const cardsFinder = new CardsFinder({ userName, id });
 
+    try {
+        let cards;
+        switch (data.action) {
+            case "need":
+                cards = await cardsFinder.need();
+                break;
+            case "trade":
+                cards = await cardsFinder.trade();
+                break
+            case "users":
+                cards = await cardsFinder.users();
+                break;
+            default:
+                cards = {error: "Unknown action"};   
+        }
+        console.log("Results: ", cards)
 
-    let cards;
-    switch (data.action) {
-        case "need":
-            cards = await cardsFinder.need();
-            break;
-        case "trade":
-            cards = await cardsFinder.trade();
-            break
-        case "users":
-            cards = await cardsFinder.users();
-            break;
-        default:
-            cards = {error: "Unknown action"};   
+        socket.emit("searchResults", {
+            requestId: data.requestId,
+            action: data.action,
+            userId: id,
+            userName: userName,
+            result: cards,
+            status: "success"
+        });
+    } catch {
+        socket.emit("searchResults", {
+            requestId: data.requestId,
+            action: data.action,
+            userId: id,
+            userName: userName,
+            result: {error: "Something went wrong"},
+            status: "success"
+        });
     }
-
-    console.log("Results: ", cards)
-
-    socket.emit("searchResults", {
-        requestId: data.requestId,
-        action: data.action,
-        userId: id,
-        userName: userName,
-        result: cards,
-        status: "success"
-    });
 });
